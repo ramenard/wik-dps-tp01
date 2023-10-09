d30fc29d0b4eeb7c7bfe598a8ea9d9e47ca2a123
Raphael Menard <raphael.menard@dotic.fr>
2023-10-09 12:34:41 +0200

    TP01: dev_ops

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const url = '/ping';
+const port = process.env.PING_LISTEN_PORT ? process.env.PING_LISTEN_PORT : 3000;
+(0, http_1.createServer)(function (req, res) {
+    if (req.method === 'GET' && req.url === url) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(req.headers));
+    }
+    else {
+        res.writeHead(404, { "Content-Type": 'application/json' });
+    }
+    res.end();
+}).listen(port, function () {
+    console.log(`server listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { createServer, IncomingMessage, ServerResponse } from "http";
+
+const url: string = '/ping'
+const port = process.env.PING_LISTEN_PORT ? process.env.PING_LISTEN_PORT : 3000
+
+createServer(function (req: IncomingMessage, res: ServerResponse) {
+    if (req.method === 'GET' && req.url === url) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(req.headers));
+    }else {
+        res.writeHead(404, { "Content-Type": 'application/json' });
+    }
+    res.end();
+}).listen(port, function () {
+    console.log(`server listening on port ${port}`);
+});
