f2b8f601fe8f1271f35c47da8873120b337c7056
Raphael Menard <raphael.menard@dotic.fr>
2023-10-17 11:38:17 +0200

    enhancement: console log hostname when req on /ping

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ createServer(function (req: IncomingMessage, res: ServerResponse) {
     if (req.method === 'GET' && req.url === url) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify(req.headers));
+        console.log(`hostname: ${hostname}`)
     }else {
         res.writeHead(404, { "Content-Type": 'application/json' });
     }
